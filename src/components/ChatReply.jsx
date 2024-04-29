9d877a700fbb1adcd28e4fda2f541e1f24e6f8ac
Hirak <hirak.bhatta2004@gmail.com>
2024-04-29 21:15:04 +0530

    fixed some bugs

--- src/components/ChatReply.jsx
+++ src/components/ChatReply.jsx
@@ -4,7 +4,7 @@ function ChatReply({ text }) {
   return (
     <div className="chat chat-start m-4">
         <div className="chat-bubble chat-bubble-primary shadow-xl">{text == `Loading`? <span className="loading loading-dots loading-md"></span> : text}</div>
-        <div className="chat-footer opacity-50">{text == `Loading`?`Loading` : `Delivered`}</div>
+        <div className="chat-footer opacity-50">{text == `Loading`?`Typing` : `Delivered`}</div>
     </div>
   )
 }
